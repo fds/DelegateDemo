2a3851cdf3b91d0dde534b484bf18d024c809490
pony <malu19871113@126.com>
2014-05-12 10:37:20 +0800

    Initial Commit

---
+++ DelegateDemo/DelegateDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DelegateDemoViewController.h
+//  DelegateDemo
+//
+//  Created by pony ma on 5/12/14.
+//  Copyright (c) 2014 pony ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DelegateDemoViewController : UIViewController
+
+@end
---
+++ DelegateDemo/DelegateDemoViewController.m
@@ -0,0 +1,29 @@
+//
+//  DelegateDemoViewController.m
+//  DelegateDemo
+//
+//  Created by pony ma on 5/12/14.
+//  Copyright (c) 2014 pony ma. All rights reserved.
+//
+
+#import "DelegateDemoViewController.h"
+
+@interface DelegateDemoViewController ()
+
+@end
+
+@implementation DelegateDemoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
